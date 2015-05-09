180bb2f585822f385773ad4422f7f46e5c165dd4
Nishant Nayudu <nishant.nayudu@gmail.com>
2015-05-08 20:29:39 -0500

    latest classifier

--- read_email.py
+++ read_email.py
@@ -28,7 +28,7 @@ def parseFolder(foldername):
 			if payload != None:
 				try:
 					features = str(payload)
-					feature_list.append(str('Nigga'))
+					feature_list.append(unicode((features), errors='replace'))
 				except ValueError:
 					null_count += 1
 					print len(payload)
@@ -49,8 +49,6 @@ for foldername, value in folders:
 	feature_list.extend(result)
 	y.extend([value] * len(result))
 
-print "LENGTH: ", len(y)
-
 # Shuffle 
 n = len(y)
 feature_list_shuf = []
@@ -58,24 +56,20 @@ y_shuf = []
 index_shuf = range(n)
 shuffle(index_shuf)
 for i in index_shuf:
-    feature_list_shuf.append(feature_list)
-    y_shuf.append(y)
+    feature_list_shuf.append(feature_list[i])
+    y_shuf.append(y[i])
 
 # Partition
-partition = 0.5
+partition = 0.25
 size_training = int(math.floor(partition*n))
 
-vectorizer = CountVectorizer(analyzer = "word",   \
-                             tokenizer = None,    \
-                             preprocessor = None, \
-                             stop_words = None,   \
-                             max_features = 5000)
+vectorizer = CountVectorizer()
 processed_list = vectorizer.fit_transform(feature_list_shuf)
 
 training_x = processed_list[0:size_training]
-training_y = y[0:size_training]
+training_y = y_shuf[0:size_training]
 test_x = processed_list[size_training:n]
-test_y = y[size_training:n]
+test_y = y_shuf[size_training:n]
 
 #Train classifier
 clf = svm.SVC()
@@ -85,4 +79,4 @@ clf.fit(training_x, training_y)
 prediction = clf.predict(test_x)
 correct = [i for i,j in zip(prediction, test_y) if i==j]
 correct = len(correct)
-print correct*100/len(test_x)
+print correct*100/len(test_y)
